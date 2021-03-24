e2897477ab4e0ebd185a0615453c9ac97fe97711
pattypatpat2632 <oleary.audio@gmail.com>
2021-03-24 12:37:05 -0400

    first commit

---
+++ PatBudget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PatBudget
+//
+//  Created by Patrick O'Leary on 3/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PatBudget/PatBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatBudgetApp.swift
+//  PatBudget
+//
+//  Created by Patrick O'Leary on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct PatBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
