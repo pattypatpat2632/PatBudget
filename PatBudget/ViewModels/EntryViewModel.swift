394e8186cd6b53afd73621ba25bab0b8be7978bd
pattypatpat2632 <oleary.audio@gmail.com>
2021-03-30 10:06:52 -0400

    add search entry

--- PatBudget/ViewModels/EntryViewModel.swift
+++ PatBudget/ViewModels/EntryViewModel.swift
@@ -14,8 +14,32 @@ final class EntryViewModel: ObservableObject {
     @Published var creditAccount = ""
     @Published var debitAccount = ""
     @Published var value = ""
+    @Published var previousTransactions = [RecordedTransaction]()
+    private var filteredAccounts: Set<String> {
+        var set = Set<String>()
+        previousTransactions.forEach { transaction in
+            if !set.contains(transaction.creditAccount) {
+                set.insert(transaction.creditAccount)
+            }
+            if !set.contains(transaction.debitAccount) {
+                set.insert(transaction.debitAccount)
+            }
+        }
+        return set
+    }
+    
+    var creditAccountOptions: [String] {
+        return search(set: filteredAccounts, forText: creditAccount)
+    }
+    
+    var debitAccountOptions: [String] {
+        return search(set: filteredAccounts, forText: debitAccount)
+    }
+    
+    private var bag = Set<AnyCancellable>()
     
     func didEnterTransaction() {
+        guard creditAccount != debitAccount else {return}
         guard let uid = Auth.auth().currentUser?.uid else {return}
         guard let creditValue = Double(value) else {return}
         guard let debitValue = Double(value) else {return}
@@ -27,6 +51,60 @@ final class EntryViewModel: ObservableObject {
                                       date: Date())
         
         DataStore.shared.record(transaction: transaction, in: uid)
+    }
+    
+    func viewDidAppear() {
+        guard let uid = Auth.auth().currentUser?.uid else {return}
+        DataStore.shared.observeTransactions(in: uid).sink { (_) in
+            
+        } receiveValue: { (transactions) in
+            self.previousTransactions = transactions
+        }.store(in: &bag)
+    }
+    
+    func search(set: Set<String>, forText text: String) -> [String] {
+        let strippedText = text.trimmingCharacters(in: .whitespaces)
+        guard !strippedText.isEmpty else {return []}
+        
+        let matcher = SearchMatcher(searchString: strippedText)
+        
+        let searchedSet = set.filter { accountName in
+            return matcher.matches(accountName)
+        }
+        
+        return Array(searchedSet)
+    }
+    
+    struct SearchMatcher {
+        
+        private(set) var searchTokens: [String.SubSequence]
+        
+        init(searchString: String) {
+            self.searchTokens = searchString
+                .split(whereSeparator: {$0.isWhitespace})
+                .sorted{ $0.count <= $1.count }
+        }
+        
+        func matches(_ candidateString: String) -> Bool {
+            guard !searchTokens.isEmpty else { return true }
+            var candidateStringTokens = candidateString.split(whereSeparator: { $0.isWhitespace })
+            
+            for searchToken in searchTokens {
+                var matchedSearchToken = false
+                
+                for (candidateStringTokenIndex, candidateStringToken) in candidateStringTokens.enumerated() {
+                    if let range = candidateStringToken.range(of: searchToken, options: [.caseInsensitive, .diacriticInsensitive]),
+                       range.lowerBound == candidateStringToken.startIndex {
+                        matchedSearchToken = true
+                        
+                        candidateStringTokens.remove(at: candidateStringTokenIndex)
+                        break
+                    }
+                }
+                guard matchedSearchToken else { return false }
+            }
+            return true
+        }
     }
 }
 
@@ -42,6 +120,22 @@ class DataStore {
         ref.child("users").child(uid).child("email").setValue(email)
     }
     
+    func observeTransactions(in uid: String) -> AnyPublisher<[RecordedTransaction], Error> {
+        let subject = PassthroughSubject<[RecordedTransaction], Error>()
+        ref.child("users").child(uid).child("transactions").observe(.value) { (snapshot) in
+            var transactions = [RecordedTransaction]()
+            let dict = snapshot.value as? [String: Any] ?? [:]
+            dict.keys.forEach { key in
+                let subDict = dict[key] as? [String: Any] ?? [:]
+                if let transaction = RecordedTransaction(id: key, subDict) {
+                    transactions.append(transaction)
+                }
+            }
+            subject.send(transactions)
+        }
+        return subject.eraseToAnyPublisher()
+    }
+    
     func record(transaction: Transaction, in uid: String) {
         ref.child("users").child(uid).child("transactions").childByAutoId().setValue([
             "debitAccount": transaction.debitAccount,
@@ -53,10 +147,44 @@ class DataStore {
     }
 }
 
-struct Transaction {
-    let debitAccount: String
-    let debitValue: Double
-    let creditAccount: String
-    let creditValue: Double
-    let date: Date
+protocol Entry {
+    var debitAccount: String {get}
+    var debitValue: Double {get}
+    var creditAccount: String {get}
+    var creditValue: Double {get}
+    var date: Date {get}
+}
+
+struct Transaction: Entry {
+    var debitAccount: String
+    var debitValue: Double
+    var creditAccount: String
+    var creditValue: Double
+    var date: Date
+}
+
+struct RecordedTransaction: Entry, Identifiable {
+    var id: String
+    var debitAccount: String
+    var debitValue: Double
+    var creditAccount: String
+    var creditValue: Double
+    var date: Date
+}
+
+extension RecordedTransaction {
+    init?(id: String, _ dict: [String: Any]) {
+        self.id = id
+        guard let debitAccount = dict["debitAccount"] as? String else {return nil}
+        guard let debitValue = dict["debitValue"] as? Double else {return nil}
+        guard let creditAccount = dict["creditAccount"] as? String else {return nil}
+        guard let creditValue = dict["creditValue"] as? Double else {return nil}
+        guard let date = dict["date"] as? TimeInterval else {return nil}
+        
+        self.debitAccount = debitAccount
+        self.debitValue = debitValue
+        self.creditAccount = creditAccount
+        self.creditValue = creditValue
+        self.date = Date(timeIntervalSince1970: date)
+    }
 }
--- PatBudget/Views/EntryView.swift
+++ PatBudget/Views/EntryView.swift
@@ -10,18 +10,59 @@ import SwiftUI
 struct EntryView: View {
     
     @ObservedObject var viewModel = EntryViewModel()
-    
+    @State private var isEditingCreditAccount = false
+    @State private var isEditingDebitAccount = false
     
     var body: some View {
         VStack {
-            TextField("Credit", text: $viewModel.creditAccount)
+            TextField("Credit", text: $viewModel.creditAccount) { (didStartEditing) in
+                if didStartEditing { isEditingCreditAccount = true }
+            } onCommit: {
+                isEditingCreditAccount = false
+            }
+            
+            if isEditingCreditAccount {
+                ScrollView {
+                    LazyVStack {
+                        ForEach(viewModel.creditAccountOptions, id: \.self) { account in
+                            Button(account) {
+                                viewModel.creditAccount = account
+                                isEditingCreditAccount = false
+                            }
+                            
+                        }
+                    }
+                }
+            }
+            
             TextField("Credit Amount", text: $viewModel.value)
                 .keyboardType(.decimalPad)
-            TextField("Debit", text: $viewModel.debitAccount)
+            
+            TextField("Debit", text: $viewModel.debitAccount) { (didStartEditing) in
+                if didStartEditing { isEditingDebitAccount = true }
+            } onCommit: {
+                isEditingDebitAccount = false
+            }
+            
+            if isEditingDebitAccount {
+                ScrollView {
+                    LazyVStack {
+                        ForEach(viewModel.debitAccountOptions, id: \.self) { account in
+                            Button(account) {
+                                viewModel.debitAccount = account
+                                isEditingDebitAccount = false
+                            }
+                        }
+                    }
+                }
+            }
+            
             TextField("Debit Amount", text: $viewModel.value)
                 .keyboardType(.decimalPad)
+            
             Button("Enter", action: viewModel.didEnterTransaction)
-        }
+            Text("Total transactions: \(viewModel.previousTransactions.count)")
+        }.onAppear(perform: viewModel.viewDidAppear)
     }
 }
 
@@ -30,8 +71,6 @@ struct EntryView_Previews: PreviewProvider {
         EntryView()
     }
 }
-
-
 
 struct Account: Identifiable, Hashable {
     let name: String
